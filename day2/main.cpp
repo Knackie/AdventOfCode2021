24cfd212b484282a8d2426ad724ded8c7ec0681b
Knackie <43431712+Knackie@users.noreply.github.com>
2021-12-10 14:09:19 +0100

    Update main.cpp

--- day2/main.cpp
+++ day2/main.cpp
@@ -1,43 +1,68 @@
-#include <iostream>
-#include <stdlib.h>
-#include <fstream>
 #include <string>
-#include <vector>
-using namespace std;
+#include <iostream>
+#include <fstream>
+#include <array>
 
-int main()
-{
-	ifstream fichier("input.txt");
-	vector <string> number;
-	int forward = 0;
-	int depth = 0;
+int task1(std::ifstream& file) {
+    int depth = 0;
+    int horizontal = 0;
+    std::string direction;
+    int amount;
 
-	if (fichier)
-	{
-		//L'ouverture s'est bien passée, on peut donc lire
+    while (file >> direction) {
+        file >> amount;
+        if (direction == "forward") {
+            horizontal += amount;
+        } else if (direction == "down") {
+            depth += amount;
+        } else if (direction == "up") {
+            depth -= amount;
+        } else {
+            std::cerr << "unrecognized input '" << direction << "'" << std::endl;
+        }
+    }
 
-		string ligne; //Une variable pour stocker les lignes lues
+    return depth*horizontal;
+}
 
-		while (getline(fichier, ligne)) //Tant qu'on n'est pas à la fin, on lit
-		{
-			number.push_back(ligne);
-		}
-	}
-	else
-	{
-		cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
-	}
-	cout << "end read of file \n"; 
+int task2(std::ifstream& file) {
+    int aim = 0;
+    int depth = 0;
+    int horizontal = 0;
+    std::string direction;
+    int amount;
 
-	for (int i = 0; i < number.size(); i++)
-	{
-		if (number[i].size() == 9)
-		{
-			forward += atoi(number[i][8]);
-		}
+    while (file >> direction) {
+        file >> amount;
+        if (direction == "forward") {
+            horizontal += amount;
+            depth += aim * amount;
+        } else if (direction == "down") {
+            aim += amount;
+        } else if (direction == "up") {
+            aim -= amount;
+        } else {
+            std::cerr << "wtf '" << direction << "'" << std::endl;
+        }
+    }
 
-	}
-	cout << forward << endl;
-	return 0;
+    return depth*horizontal;
+}
+int main() {
+    std::ifstream file;
+    file.open("input.txt");
+
+    int result1 = task1(file);
+
+    file.clear();
+    file.seekg(0, std::ios::beg);
+
+    int result2 = task2(file);
+
+    file.close();
+
+    std::cout << "Task1: " << result1 << "\n";
+    std::cout << "Task2: " << result2 << std::endl;
+    return 0;
 }
 
